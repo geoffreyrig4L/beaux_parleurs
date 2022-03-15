492eef157847eb76ec7a0fe2126f3cb48e07e1cb
geoffreyrig4l <grigal@oui.sncf>
2022-03-15 15:49:43 +0100

    separat° front et back + knexfile

---
+++ backend/migrations/20220315142312_initial-schema.js
@@ -0,0 +1,33 @@
+export const up = async (knex) => {
+  await knex.schema.createTable("users", (table) => {
+    table.increments("id")
+    table.text("firstName").notNullable()
+    table.text("lastName").notNullable()
+    table.text("email").notNullable().unique()
+    table.text("passwordHash").notNullable()
+    table.text("passwordSalt").notNullable()
+    table.date("birthdate").notNullable()
+    table.text("addressLine").notNullable()
+    table.text("city").notNullable()
+    table.text("zipcode").notNullable()
+    table.text("country").notNullable()
+    table.text("phone").notNullable().unique()
+    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
+  })
+  await knex.schema.createTable("topics", (table) => {
+    table.increments("id")
+    table.text("name").notNullable()
+    table.text("creationDate").notNullable()
+  })
+  await knex.schema.createTable("comments", (table) => {
+    table.increments("id")
+    table.text("content").notNullable()
+    table.text("sendingDate").notNullable()
+  })
+}
+
+export const down = async (knex) => {
+  await knex.schema.dropTable("users")
+  await knex.schema.dropTable("topics")
+  await knex.schema.dropTable("comments")
+}
---
+++ backend/src/db/migration.stub.js
@@ -0,0 +1,8 @@
+/*eslint-disable*/
+export const up = async (knex) => {
+  await 
+}
+
+export const down = async (knex) => {
+  await
+}
