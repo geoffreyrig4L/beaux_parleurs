3623a14709cded41f5e3aebd8cd5d268f6250465
geoffreyrig4l <grigal@oui.sncf>
2022-04-08 14:30:36 +0200

    checkpoint addcomment

--- frontend/src/components/AddComment.jsx
+++ frontend/src/components/AddComment.jsx
@@ -1,58 +1,91 @@
 import AppContext from "./AppContext"
-import { useCallback, useContext, useState } from "react"
+import { Formik, Field } from "formik"
+import { useCallback, useContext, useState, useEffect } from "react"
 import api from "../services/api"
-import { FormField, Formik, Form } from "formik"
 
 const AddComment = ({ addComment, titre, action, commentaire, sujetId }) => {
-  const [session, router] = useContext(AppContext)
+  const { session, router } = useContext(AppContext)
   const [apiError, setApiError] = useState(null)
+  const [utilisateur, setUtilisateur] = useState(0)
 
-  let idUtilisateur = ""
-  if (session) {
-    idUtilisateur = JSON.parse(session).payload.utilisateur.id
-  }
+  useEffect(() => {
+    if (session) {
+      setUtilisateur(JSON.parse(session).payload.utilisateur.id)
+    }
+  })
 
-  const publier = useCallback(
+  const handleFormSubmit = useCallback(
     async ({ contenu }) => {
       try {
+        const con = "petasse"
         const like = 0
-        const dateCreation = ""
+        const dateCreation = "8====D"
+        console.log(
+          contenu +
+            " ----- " +
+            con +
+            " ----- " +
+            like +
+            " ----- " +
+            dateCreation +
+            " ----- " +
+            utilisateur +
+            " ----- " +
+            sujetId
+        )
         if (action === "creer") {
+          console.log("1 " + action)
           await api.post("/commentaires", {
-            contenu,
+            con,
             like,
             dateCreation,
-            idUtilisateur,
+            utilisateur,
             sujetId,
           })
-          router.push("/")
+        } else if (action === "modifier") {
+          console.log("2 " + action)
+          await api.put(`/commentaires/${commentaire.id}`, {
+            con,
+          })
         }
       } catch (err) {
         setApiError(err.response.data.error)
         console.log(apiError)
       }
     },
-    [router, idUtilisateur, sujetId, action, apiError]
+    [utilisateur, sujetId, action, apiError, commentaire.id]
   )
+
+  const textArea = () => {
+    return (
+      <textarea
+        className="pl-[12px] py-2 bg-gray-50 h-24 max-h-[250px] min-h-[100px]"
+        required
+      >
+        {commentaire.contenu}
+      </textarea>
+    )
+  }
 
   return addComment ? (
     <div className="mt-8">
-      <form className="flex flex-col mb-6">
-        <label className="mr-[20px] font-bold">{titre}</label>
-        <textArea
-          id="contenuCommentaire"
-          className="pl-[12px] py-2 bg-gray-50 h-24 max-h-[250px] min-h-[100px]"
-          required
-        >
-          {commentaire.contenu}
-        </textArea>
-        <button
-          className="bg-gray-100 w-full h-8 hover:bg-teal-600 hover:text-white"
-          onClick={publier}
-        >
-          Publier
-        </button>
-      </form>
+      <Formik
+        initialValues={{ contenu: commentaire.contenu }}
+        onSubmit={handleFormSubmit}
+      >
+        {({ handleSubmit }) => (
+          <form className="flex flex-col mb-6" onSubmit={handleSubmit}>
+            <label className="mr-[20px] font-bold">{titre}</label>
+            <Field as={textArea} name="contenu" id="contenu" />
+            <button
+              className="bg-gray-100 w-full h-8 hover:bg-teal-600 hover:text-white"
+              type="submit"
+            >
+              Publier
+            </button>
+          </form>
+        )}
+      </Formik>
     </div>
   ) : (
     <div className="italic text-gray-400 text-sm mt-8">
